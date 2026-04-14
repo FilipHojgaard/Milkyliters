e459996958435349be9a14d2f1a3e1c90e79db42
Filip Højgaard <filip.hojgaard@gmail.com>
2026-04-14 18:28:13 +0200

    Started work on the EditFeedingViewModel

---
+++ Milkyliters/Milkyliters/ViewModels/EditFeedingViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Milkyliters.Models;
+using Milkyliters.Services;
+
+namespace Milkyliters.ViewModels;
+
+[QueryProperty(nameof(FeedingId), "feedingId")]
+public partial class EditFeedingViewModel : ObservableObject
+{
+    private readonly IFeedingService _feedingService;
+
+    [ObservableProperty]
+    public partial int FeedingId { get; set; }
+
+    [ObservableProperty]
+    public partial int UpdateMl { get; set; }
+
+    [ObservableProperty]
+    public partial DateTime UpdateTimestamp { get; set; }
+
+    [ObservableProperty]
+    public partial MilktypeEnum UpdateMilkType{ get; set; }
+
+    [ObservableProperty]
+    public partial string? AddMlErrorMessage { get; set; }
+
+    public EditFeedingViewModel(IFeedingService feedingService)
+    {
+        _feedingService = feedingService;
+    }
+
+    [RelayCommand]
+    public async Task UpdateFeedingAsync()
+    {
+        if (UpdateMl <= 0)
+        {
+            AddMlErrorMessage = "Ml skal være over 0";
+            return;
+        }
+        await _feedingService.UpdateFeedingAsync(FeedingId, UpdateMl, UpdateTimestamp, UpdateMilkType);
+    }
+
+}
